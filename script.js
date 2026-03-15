a68a4e9d4917ebcb4429588502c1c8b24ba2ee7f
ktg17 <ktg7studio@gmail.com>
2026-03-15 15:19:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ Ice-cream 🍦, peaceful walk and your shy smile 😊
 
 <div class="stars"></div>
 
-<canvas id="confetti"></canvas>
 
 `
 
@@ -148,61 +147,6 @@ type()
 
 }
 
-
-
-/* CONFETTI */
-
-function startConfetti(){
-
-const canvas=document.getElementById("confetti")
-const ctx=canvas.getContext("2d")
-
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
-
-let confetti=[]
-
-for(let i=0;i<120;i++){
-
-confetti.push({
-x:Math.random()*canvas.width,
-y:Math.random()*canvas.height,
-r:Math.random()*6+2,
-d:Math.random()*50
-})
-
-}
-
-function draw(){
-
-ctx.clearRect(0,0,canvas.width,canvas.height)
-
-ctx.fillStyle="white"
-
-confetti.forEach(c=>{
-
-ctx.beginPath()
-ctx.arc(c.x,c.y,c.r,0,Math.PI*2)
-ctx.fill()
-
-})
-
-update()
-
-}
-
-function update(){
-
-confetti.forEach(c=>{
-c.y+=2
-if(c.y>canvas.height) c.y=0
-})
-
-}
-
-setInterval(draw,20)
-
-}
 
 
 // /* ICECREAM RAIN */
